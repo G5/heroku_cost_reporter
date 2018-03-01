623cc9251d444331be19f255d942aa2978b63518
Sheena Gygax <sheena.gygax@getg5.com>
2018-03-01 13:14:55 -0800

    change class method syntax

--- lib/heroku/request.rb
+++ lib/heroku/request.rb
@@ -1,30 +1,28 @@
 class Request
-  class << self
-    def where(resource_path, cache_params, options = {})
-      response, status = get_json(resource_path, cache_params)
-      status == 200 ? response : errors(response)
+  def self.where(resource_path, cache_params, options = {})
+    response, status = get_json(resource_path, cache_params)
+    status == 200 ? response : errors(response)
+  end
+
+  # def self.get(id, cache_params)
+    # response, status = get_json(id, cache_params)
+    # status == 200 ? response : errors(response)
+  # end
+
+  def self.errors(response)
+    error = { errors: { status: response['status'], message: response['message'] } }
+    response.merge(error)
+  end
+
+  def self.get_json(root_path, cache_params)
+    response =  Rails.cache.fetch(root_path, cache_params) do
+      api.get(root_path)
     end
 
-    def get(id, cache_params)
-      response, status = get_json(id, cache_params)
-      status == 200 ? response : errors(response)
-    end
+    [JSON.parse(response.body), response.status]
+  end
 
-    def errors(response)
-      error = { errors: { status: response['status'], message: response['message'] } }
-      response.merge(error)
-    end
-
-    def get_json(root_path, cache_params)
-      response =  Rails.cache.fetch(root_path, cache_params) do
-        api.get(root_path)
-      end
-
-      [JSON.parse(response.body), response.status]
-    end
-
-    def api
-      Connection.api
-    end
+  def self.api
+    Connection.api
   end
 end
