6bf76d87fae51c9cf9401924f8218702bb9a4012
Sheena Gygax <sheena.gygax@getg5.com>
2018-03-12 15:04:35 -0700

    logo, schema, comment out unused service for now

--- app/services/heroku/account.rb
+++ app/services/heroku/account.rb
@@ -1,16 +1,17 @@
-module Heroku
-  class Account < Base
+# Not sure if it woulld be useful to display an account specific info in this app commenting out for now
+# module Heroku
+  # class Account < Base
 
-    attr_accessor :email,
-                  :name
+    # attr_accessor :email,
+                  # :name
 
-    def self.info
-      response = Request.where('account')
-      Account.new(response)
-    end
+    # def self.info
+      # response = Request.where('account')
+      # Account.new(response)
+    # end
 
-    def initialize(args = {})
-      super(args)
-    end
-  end
-end
+    # def initialize(args = {})
+      # super(args)
+    # end
+  # end
+# end
