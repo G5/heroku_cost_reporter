9dee27163345484f22bac3c4fe9b5477ea9a96d2
Sheena Gygax <sheena.gygax@getg5.com>
2018-03-12 15:03:14 -0700

    don't need this

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,7 +26,7 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fabricators"
+  # config.fixture_path = "#{::Rails.root}/spec/fabricators"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
